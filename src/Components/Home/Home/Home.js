6dbaee1cb256713dbc7feb175290856113da9afc
milonahmedshuvo <milonahmedshuvo01@gmail.com>
2023-01-25 13:36:49 +0600

    home layout

--- src/Components/Home/Home/Home.js
+++ src/Components/Home/Home/Home.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 
+
+
+
+
+
 const Home = () => {
     return (
-        <div>
-            <Banner></Banner>
+        <div className='flex'>
+
+
+           <div className='border w-3/4 border-red-400'>
+            <h1>Home page all product catagoris show</h1>
+           </div>
+
+
+
+
+
+
+           <div className='border w-1/4 border-violet-500'>
+              <h1>Manu bar</h1>
+           </div>
+
+
+
+
+           
         </div>
     );
 };
--- src/Routes/Router.js
+++ src/Routes/Router.js
@@ -3,7 +3,10 @@ import Home from "../Components/Home/Home/Home";
 import Main from "../Layout/Main";
 
 export const router = createBrowserRouter([
-    {path: '/', element: <Main></Main>, children: [
+    {path: '/',
+     element: <Main></Main>, 
+     children: [
         {path: '/', element: <Home></Home>}
+        
     ]}
 ])
