5d14c83de3b7cbd1d138ba83a53d15ee1ed233f8
milonahmedshuvo <milonahmedshuvo01@gmail.com>
2023-01-27 00:45:03 +0600

    manu items

--- src/Components/Home/Manu/Manue.js
+++ src/Components/Home/Manu/Manue.js
@@ -1,9 +1,36 @@
 import React from 'react';
-
+import burger from '../../../ManuImage/Burger.jpeg';
+import burger2 from '../../../ManuImage/buger2-removebg-preview.png'
 const Manue = () => {
+
+
     return (
-        <div>
-            <h1>Manue catagoriy</h1>
+        <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-4 my-6'>
+           
+           <div>
+              <img src={burger2} alt="" />
+           </div>
+
+           <div>
+              <img src={burger2} alt="" />
+           </div>
+
+           <div>
+              <img src={burger2} alt="" />
+           </div>
+
+           <div>
+              <img src={burger2} alt="" />
+           </div>
+
+           <div>
+              <img src={burger2} alt="" />
+           </div>
+
+           <div>
+              <img src={burger2} alt="" />
+           </div>
+
         </div>
     );
 };
--- src/Routes/Router.js
+++ src/Routes/Router.js
@@ -7,6 +7,6 @@ export const router = createBrowserRouter([
      element: <Main></Main>, 
      children: [
         {path: '/', element: <Home></Home>}
-        
+         
     ]}
 ])
