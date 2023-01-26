bd6de48e8f26eaca367f10d5613b73d2397b4f10
milonahmedshuvo <milonahmedshuvo01@gmail.com>
2023-01-27 02:29:13 +0600

    item

--- src/Components/Home/Manu/Manue.js
+++ src/Components/Home/Manu/Manue.js
@@ -1,36 +1,28 @@
 import React from 'react';
 import burger from '../../../ManuImage/Burger.jpeg';
-import burger2 from '../../../ManuImage/buger2-removebg-preview.png'
+import burger2 from '../../../ManuImage/buger2-removebg-preview.png';
+import { IconName, FaAngleRight } from "react-icons/fa";
+
 const Manue = () => {
 
+    const imgs=[
+        {name:'Burger', image:burger2, icon:FaAngleRight},
+        {name:'Pizza', image:burger2, icon:FaAngleRight},
+        {name:'Ice crem', image:burger2, icon:FaAngleRight},
+        {name:'photato', image:burger2, icon:FaAngleRight},
+        {name:'Burger', image:burger2, icon:FaAngleRight},
+        {name:'Burger', image:burger2, icon:FaAngleRight},
+    ]
+
+
+
+//  <img className='w-44 hover:bg-violet-400'  src={im} key={i} alt="" />
 
     return (
-        <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-4 my-6'>
-           
-           <div>
-              <img src={burger2} alt="" />
-           </div>
-
-           <div>
-              <img src={burger2} alt="" />
-           </div>
-
-           <div>
-              <img src={burger2} alt="" />
-           </div>
-
-           <div>
-              <img src={burger2} alt="" />
-           </div>
-
-           <div>
-              <img src={burger2} alt="" />
-           </div>
-
-           <div>
-              <img src={burger2} alt="" />
-           </div>
-
+        <div className=' w-full  flex flex-row overflow-y-auto  lg:overflow-hidden'>                 
+             {
+                imgs.map((item, i)=> <img className='w-44' src={item.image} alt="" /> )
+             } 
         </div>
     );
 };
