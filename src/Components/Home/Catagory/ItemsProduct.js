16bd31e3af6604bea4d7699fc647282efa24abd3
milonahmedshuvo <milonahmedshuvo01@gmail.com>
2023-03-06 15:13:29 +0600

    items

---
+++ src/Components/Home/Catagory/ItemsProduct.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const ItemsProduct = () => {
+
+
+
+    return (
+        <div>
+            <h1> items products </h1>
+        </div>
+    );
+};
+
+export default ItemsProduct;
--- src/Routes/Router.js
+++ src/Routes/Router.js
@@ -1,4 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
+import ItemsProduct from "../Components/Home/Catagory/ItemsProduct";
 import Home from "../Components/Home/Home/Home";
 import Main from "../Layout/Main";
 
@@ -6,7 +7,17 @@ export const router = createBrowserRouter([
     {path: '/',
      element: <Main></Main>, 
      children: [
-        {path: '/', element: <Home></Home>}
+        {
+            path: '/',
+            element: <Home></Home>
+        },
+        {
+            path: '/',
+            element: <ItemsProduct></ItemsProduct>,
+            loader: async ( {params} ) =>{
+                return fetch(`http://localhost:5000/itemsProducts/${params.title}`)
+            }
+        }
          
     ]}
 ])
