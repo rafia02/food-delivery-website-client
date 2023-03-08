2f7c26724a4d2368f5008a3743a8769991f029e5
milonahmedshuvo <milonahmedshuvo01@gmail.com>
2023-03-09 00:35:57 +0600

    section

--- src/Components/Home/Count/Count.js
+++ src/Components/Home/Count/Count.js
@@ -3,9 +3,40 @@ import React from 'react';
 
 
 const Count = () => {
+
+
+
     return (
-        <div>
-           <h1> react Count </h1> 
+        <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4'>
+            <div>
+                 <img src="https://cdn.shopify.com/s/files/1/0267/5979/0615/files/icon-12.png?v=1614334585" alt="" />
+                 <h1>Menu Types</h1>
+                 <h2>200</h2>    
+            </div> 
+
+
+            <div>
+                 <img src="https://cdn.shopify.com/s/files/1/0267/5979/0615/files/icon-12.png?v=1614334585" alt="" />
+                 <h1>Menu Types</h1>
+                 <h2>200</h2>    
+            </div> 
+
+
+            <div>
+                 <img src="https://cdn.shopify.com/s/files/1/0267/5979/0615/files/icon-12.png?v=1614334585" alt="" />
+                 <h1>Menu Types</h1>
+                 <h2>200</h2>    
+            </div> 
+
+
+            <div>
+                 <img src="https://cdn.shopify.com/s/files/1/0267/5979/0615/files/icon-12.png?v=1614334585" alt="" />
+                 <h1>Menu Types</h1>
+                 <h2>200</h2>    
+            </div> 
+
+
+
         </div>
     );
 };
