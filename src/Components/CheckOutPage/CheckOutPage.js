74b1f25ebb81d328edaba1f563ff0b1115896626
milonahmedshuvo <milonahmedshuvo01@gmail.com>
2023-03-09 16:31:09 +0600

    checkout page

---
+++ src/Components/CheckOutPage/CheckOutPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+const CheckOutPage = () => {
+    return (
+        <div>
+            <h1> Check out page </h1>
+        </div>
+    );
+};
+
+export default CheckOutPage;
--- src/Components/Home/Choose/Choose.js
+++ src/Components/Home/Choose/Choose.js
@@ -5,7 +5,7 @@ const Choose = () => {
 
     return (
         <div className='mt-16'>
-           <h1 className='text-4xl font-serif mt-10 text-center '> Choose a Way to Support Food First </h1>
+           <h1 className='text-4xl font-serif mt-10 text-center '> WHY SHOP WITH US? </h1>
 
 
              
@@ -15,20 +15,20 @@ const Choose = () => {
 
                        <div className='bg-[#ffffff] shadow-lg py-10 px-8 rounded-tl-[80px] hover:rounded-tl-none rounded-br-[80px] hover:rounded-br-none ease-linear duration-700  hover:rounded-tr-[80px] hover:rounded-bl-[80px] '>
                            <img className='w-15 h-12 mb-6  ' src="https://foodfirst.org/wp-content/uploads/2022/04/support_icons_1.png" alt="" />
-                           <h1 className='text-rose-400 font-bold' >ONE-TIME GIFT</h1>
-                           <p className='font-normal mt-2 text-[#667687]' >We'll put your tax deductible gift to work right away to support grassroots movements and organizers.</p>
+                           <h1 className='text-rose-400 font-bold' >Healthy Food Habit</h1>
+                           <p className='font-normal mt-2 text-[#667687]' >Building a society & community with sound mind, mentality and health by developing healthy food habit</p>
                        </div>
 
                        <div className='bg-[#ffffff] shadow-md mt-8 pb-10 py-10 px-8    rounded-tl-[80px] hover:rounded-tl-none rounded-br-[80px] hover:rounded-br-none ease-linear duration-700  hover:rounded-tr-[80px] hover:rounded-bl-[80px] '>
                            <img className='w-15 h-12 mb-6'  src="https://foodfirst.org/wp-content/uploads/2022/04/support_icons_3.png" alt="" />
-                           <h1 className='text-rose-400 font-bold'>MONTHLY DONATION</h1>
-                           <p className='font-normal mt-2 text-[#667687]' >For as little as $3 a month you can join our Sustainer's Circle. This helps Food First plan ahead and accomplish even more.</p>
+                           <h1 className='text-rose-400 font-bold'>Environmental safety</h1>
+                           <p className='font-normal mt-2 text-[#667687]' >We always work on keeping a healthy environment for our employees. We dump and recycle our wastes in proper manner.</p>
                        </div>
 
                        <div className='bg-[#ffffff] shadow-md py-10 px-8  rounded-tl-[80px] hover:rounded-tl-none rounded-br-[80px] hover:rounded-br-none ease-linear duration-700  hover:rounded-tr-[80px] hover:rounded-bl-[80px] '>
                            <img className='w-15 h-12 mb-6'  src="https://foodfirst.org/wp-content/uploads/2022/04/support_icons_5.png" alt="" />
-                           <h1 className='text-rose-400 font-bold'>LIVING LEGACY</h1>
-                           <p className='font-normal mt-2 text-[#667687]' >With a legacy donation you can make a much larger donation than you could afford to give during your lifetime.</p>
+                           <h1 className='text-rose-400 font-bold'>Honesty & Integrity</h1>
+                           <p className='font-normal mt-2 text-[#667687]' >Our aim is to earn halal income for our stakeholders through doing business with integrity & honestly in a fair manner.</p>
                        </div>
 
 
@@ -46,14 +46,14 @@ const Choose = () => {
 
                   <div className='bg-[#ffffff] shadow-md w-1/3 py-10 px-8  rounded-tl-[80px] hover:rounded-tl-none rounded-br-[80px] hover:rounded-br-none ease-linear duration-700  hover:rounded-tr-[80px] hover:rounded-bl-[80px] '>
                            <img className='w-15 h-12 mb-6'  src="https://foodfirst.org/wp-content/uploads/2022/04/support_icons_4.png" alt="" />
-                           <h1 className='text-rose-400 font-bold'> DONATING STOCK </h1>
-                           <p className='font-normal mt-2 text-[#667687]' >Donating highly appreciated stock allows you to avoid capital gains tax on it.</p>
+                           <h1 className='text-rose-400 font-bold'> Pure and safe Grocery Store </h1>
+                           <p className='font-normal mt-2 text-[#667687]' >We focus on ensuring sound health by delivering adulteration, additives & chemical free pure and safe grocery food at consumers’ doorstep.</p>
                        </div>
                        
                        
                        <div className='bg-[#ffffff] shadow-md  w-1/3 py-10 px-8    rounded-tl-[80px] hover:rounded-tl-none rounded-br-[80px] hover:rounded-br-none ease-linear duration-700  hover:rounded-tr-[80px] hover:rounded-bl-[80px] '>
                            <img className='w-15 h-12 mb-6'  src="https://foodfirst.org/wp-content/uploads/2022/04/support_icons_6.png" alt="" />
-                           <h1 className='text-rose-400 font-bold'>FOR YOUR LOVED ONES</h1>
+                           <h1 className='text-rose-400 font-bold'>Quality Guaranteed</h1>
                            <p className='font-normal mt-2 text-[#667687]' >Who do you admire for standing up for what they believe in? Here's your chance to tell them and/or their family members.</p>
                        </div>
                   </div>
