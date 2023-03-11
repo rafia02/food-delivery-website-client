2c850d9cc2732044e2d8d17fc8c213f9187c30da
milonahmedshuvo <milonahmedshuvo01@gmail.com>
2023-03-11 13:19:10 +0600

    wishlist single

---
+++ src/Components/Wishlists/WishlistSingle.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const WishlistSingle = () => {
+    return (
+        <div>
+            
+            <h1> wishlists Single </h1>
+
+        </div>
+    );
+};
+
+export default WishlistSingle;
--- src/Components/Wishlists/Wishlists.js
+++ src/Components/Wishlists/Wishlists.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import React, { useContext } from 'react';
+import { Contex } from '../../Context/AuthProvider';
+import Spinner from '../Share/Spinner/Spinner';
+import WishlistSingle from './WishlistSingle';
 
 const Wishlists = () => {
+    const {user, loading}= useContext(Contex)
+    
+
+    const {data: wishlists=[], isLoading, refetch }= useQuery({
+        queryKey: ["wishlists", user?.email ],
+        queryFn: async ()=> {
+            const res= await fetch(`http://localhost:5000/wishlistsProducts?email=${user?.email}`);
+            const data= res.json()
+            return data;
+        }
+    })
+
+    console.log(wishlists)
+    if(loading){
+        return <Spinner></Spinner>
+    }
+   
+
     return (
         <div>
             <h1>Wishlists products</h1> 
+
+            {
+                wishlists.map((lists)=> <WishlistSingle
+                key={lists._id}
+                ></WishlistSingle> )
+            }
         </div>
     );
 };
